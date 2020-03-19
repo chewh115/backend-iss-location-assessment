419bbe864401d397d98cf5f8e20ee11a13f6f615
Chelsea <chewh115@gmail.com>
2020-03-19 14:51:40 -0400

    Updates to have part A working

--- iss.py
+++ iss.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'chewh115'
+
+import requests
+
+
+def get_names_of_astros():
+    """Gets list of astronauts currently in space, the spacecraft they're on,
+    and how many astronauts are in space from website data"""
+    r = requests.get('http://api.open-notify.org/astros.json')
+    astronauts = r.json()
+    for astro in astronauts['people']:
+        print('{} is currently on the {}'.format(
+            astro['name'], astro['craft']))
+    print('There are currently {} astronauts in the cosmos.'.format(
+        len(astronauts['people'])))
 
 
 def main():
-    pass
+    get_names_of_astros()
 
 
 if __name__ == '__main__':
