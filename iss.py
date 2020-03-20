998fd9c423cbaad831368486fc292f6c39cd51d0
Chelsea <chewh115@gmail.com>
2020-03-20 09:54:32 -0400

    Updates to have ISS station correctly showing with no errors, Indy location still isn't working.

--- iss.py
+++ iss.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
+"""This module seeks to find out information about the ISS, including who's
+    currently on board, where it is on a map,
+    and when it will next pass Indianapolis"""
+
 __author__ = 'chewh115'
 
 import requests
 import time
+import turtle
 
 
 def get_names_of_astros():
@@ -29,11 +34,35 @@ def get_iss_coordinates():
     timestamp = time.ctime(timestamp)
     print('As of {}, the ISS\'s longitude is {}, and latitude is {}'.format(
         timestamp, longitude, latitude))
+    return latitude, longitude
+
+
+def show_on_map(coordinates):
+    """Draws the location of ISS on a map"""
+    world_map = turtle.Screen()
+    world_map.setup(width=720, height=360)
+    world_map.bgpic('map.gif')
+    world_map.setworldcoordinates(-180, -180, 180, 180)
+    world_map.register_shape('iss.gif')
+    iss_station = turtle.Turtle()
+    iss_station.shape('iss.gif')
+    iss_station.penup()
+    iss_station.goto(float(coordinates[0]), float(coordinates[1]))
+    return world_map
+
+
+def over_indy():
+    indy_location = {'Latitude': '39.7684', 'Longitude': '-86.1581'}
+    indy_pass = requests.get(
+        'http://api.open-notify.org/iss-pass.json', params=indy_location)
+    print(indy_pass.json())
 
 
 def main():
     get_names_of_astros()
-    get_iss_coordinates()
+    coordinates = get_iss_coordinates()
+    show_on_map(coordinates)
+    over_indy()
 
 
 if __name__ == '__main__':
